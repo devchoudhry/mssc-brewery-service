607527111498c29fdfb87e5848eaf39d788ac2e1
devchoudhry <devchoudhry@DESKTOP-LOK74J9>
2020-07-05 00:32:57 -0400

    Adding models for Closes #1

---
+++ src/main/java/guru/springframework/msscbreweryservice/web/model/BeerDto.java
@@ -0,0 +1,38 @@
+/**
+ * 
+ */
+package guru.springframework.msscbreweryservice.web.model;
+
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * @author devchoudhry
+ *
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class BeerDto {
+	
+	private UUID id;
+	private Integer version;
+	
+	private OffsetDateTime createdDate;
+	private OffsetDateTime lastModifiedDate;
+	private String beerName;
+	private BeerStyleEnum beerStyle;
+	
+	private Long upc;
+	private BigDecimal price;
+	
+	private Integer quantityOnHand;
+
+}
